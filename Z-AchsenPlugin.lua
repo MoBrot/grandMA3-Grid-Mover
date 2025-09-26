24b17ac7a59688294895c47099d803bbe6fd98d9
Maurice <90271578+MoBrot@users.noreply.github.com>
2025-09-26 09:21:58 +0200

    Basen Function done

--- Z-AchsenPlugin.lua
+++ Z-AchsenPlugin.lua
@@ -1,8 +1,3 @@
-local luaComponentHandle = select(4,...)
-
-fals = "NO"
-tru = "YES"
-
 variable_prefix = "z-ajustment-" 
 variable_name = variable_prefix .. "selectionconfig"
 variable_hookID = variable_prefix .. "hookID"
@@ -15,7 +10,6 @@ local function GetSelectionTable()
     local result = {}
 
     local fixtureIndex, gridX, gridY, gridZ = SelectionFirst()
-    assert(fixtureIndex, "Please select a (range of) fixture(s) and try again.")
 
     while fixtureIndex do
         table.insert(result, {
@@ -33,59 +27,41 @@ local function GetSelectionTable()
 end
 
 function ajustZForSelect(toAdd, selection)
-    
+  for _, fixture in pairs(selection) do
+    executeCommand("Grid " .. (fixture.gridX) .. "/" .. (fixture.gridY) .. "/" .. (fixture.gridZ - toAdd))
+    executeCommand("Fixture " .. fixture.asFixture.FID)
+  end
 end
 
-function setVariable(name, value)
-  SetVar(UserVars(), name, value)
-end
-
-function getVariable(name)
-  return GetVar(UserVars(), name)
-end
-
-function SelectionCallBack()
-  Printf("Selection changed!")
-  Printf(Selection().COUNTTOTALSELECTED)
+function reselectAllFixtures(selection)
+  for _, fixture in pairs(selection) do
+    executeCommand("Fixture " .. fixture.asFixture.FID)
+  end
 end
 
 function main(display, args)
-  Selection():Dump()
-
-  if args == "initHook" then
-
-    local pluginHandle = luaComponentHandle:Parent()
-    setVariable(variable_hookID, HookObjectChange(SelectionCallBack, Selection(), pluginHandle))
-    return
-  end
-
-  if args == "unhook" then
-
-    hookID = getVariable(variable_hookID)
-
-      if(hookID ~= "") then
-        Unhook()
-        setVariable(variable_hookID, "")
-      end
-    return
-  end
 
   setupToggle = Selection().SETUPMODE;
 
-  if setupToggle == fals then
+  if setupToggle == false then
     Printf("Fixtures Must nbe selected in the Selectiongrid Setup mode!")
+    return;
   end
 
   selection = GetSelectionTable()
 
   if #selection == 0 then
-    Print("Atleast one Fixture must be selected")
+    Printf("Atleast one Fixture must be selected")
+    return;
   end
 
-  Selection().SETUPMODE = fals;
+  Selection().SETUPMODE = false;
 
   ajustZForSelect(-1, selection);
 
+  Selection().SETUPMODE = true
+
+  reselectAllFixtures(selection)
 end
 
 return main
